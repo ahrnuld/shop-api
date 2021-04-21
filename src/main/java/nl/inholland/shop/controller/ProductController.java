3ec5e15d20b8c8b1a699adcd6d4b8809faf4cb97
Mark-Inholland <Mark-Inholland@DESKTOP-JVU22GG>
2021-04-21 10:20:01 +0200

    First version

---
+++ src/main/java/nl/inholland/shop/controller/ProductController.java
@@ -0,0 +1,25 @@
+package nl.inholland.shop.controller;
+
+import nl.inholland.shop.model.Product;
+import nl.inholland.shop.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
